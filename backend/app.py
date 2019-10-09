893c0fe6d9eecf8d998d807dd5272e9546a38d35
Charles Carlson <carlson.charles20@gmail.com>
2019-10-09 12:04:00 -0700

    fresh push

--- backend/app.py
+++ backend/app.py
@@ -1,10 +1,12 @@
 from flask import Flask, request
 import subprocess, sys
 import docker
+from flask_pymongo import PyMongo
 
 app = Flask(__name__)
 app.secret_key = "SUPER SECRET KEY"
-
+app.config["MONGO_URI"] = "mongodb://localhost:27017/Launch"
+mongo = PyMongo(app)
 @app.route('/')
 def home():
     return ("<h1>Hello, World!</h1>")
@@ -17,18 +19,21 @@ def api(query):
 @app.route('/deploy', methods=['POST'])
 def deploy():
     json_data = request.get_json()
-    repo = json_data['repo']
-    user = json_data['user']
-    github_string = 'git@github.com:{}/{}.git'.format(user,repo)
-    if sys.platform.startswith('linux') or sys.platform.startswith('darwin'):
-        subprocess.call(['mkdir', '/home/{}/{}'.format(user,repo)])
-        subprocess.call(['git','init'])
-        subprocess.call(['git', 'clone', github_string])
-        # create_image(user, repo)
-        return("cloning {}".format(github_string))
-    else:
-        print('Running on windows, most likely dev...')
-        print('Right now would be setting up ' + repo + ' from user: ' + user)
+    if request.method == "POST":
+        repo = json_data['repo']
+        user = json_data['user']
+        if repo is not None and user is not None:
+            mongo.db.
+        github_string = 'git@github.com:{}/{}.git'.format(user,repo)
+        if sys.platform.startswith('linux') or sys.platform.startswith('darwin'):
+            subprocess.call(['mkdir', '/home/{}/{}'.format(user,repo)])
+            subprocess.call(['git','init'])
+            subprocess.call(['git', 'clone', github_string])
+            # create_image(user, repo)
+            return("cloning {}".format(github_string))
+        else:
+            print('Running on windows, most likely dev...')
+            print('Right now would be setting up ' + repo + ' from user: ' + user)
 
 
 if __name__ == '__main__':
