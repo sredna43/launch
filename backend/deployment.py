53b2d442e9a738d0f57940720891b0faec4cdabd
Anders Olson <sredna43@gmail.com>
2019-10-21 20:24:20 -0500

    tag might be better now

--- backend/deployment.py
+++ backend/deployment.py
@@ -27,7 +27,8 @@ def create_image(repo, path_to_dockerfile, is_frontend=False):
     print("Creating image: {}".format(path_to_dockerfile))
     client = docker.from_env()
     path_to_dockerfile = path_to_dockerfile.replace('Dockerfile', '')
-    image = client.images.build(path=path_to_dockerfile, rm=True, tag=path_to_dockerfile)
+    tag = path_to_dockerfile.replace(homedir(), '').replace(repo, '').replace('/','')
+    image = client.images.build(path=path_to_dockerfile, rm=True, tag=tag)
     return image
 
 # This returns a list of the dockerfiles found, in the form of their file location
