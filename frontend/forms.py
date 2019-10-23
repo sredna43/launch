22abe0941c0e5384cb1e30bc61ef68e0752b42ef
Anders Olson <sredna43@gmail.com>
2019-10-23 14:13:08 -0500

    took away .pyc

--- frontend/forms.py
+++ frontend/forms.py
@@ -4,9 +4,9 @@ from wtforms.validators import ValidationError, DataRequired, Email, EqualTo, Le
 
 class GithubRepo(FlaskForm):
     # Main form for the tool
-    database_list = [('mongo', 'MongoDB'), ('postgres', 'PostgreSQL'), ('redis', 'Redis'), ('mysql', 'MySQL')]
+    database_list = [('custom', 'Custom'), ('mongo', 'MongoDB'), ('postgres', 'PostgreSQL'), ('redis', 'Redis'), ('mysql', 'MySQL')]
 
     user = StringField(label=u'Github Username:', validators=[DataRequired()])
     repo = StringField(label=u'Repository Name:', validators=[DataRequired()])
-    db = SelectField(label=u'Databases', choices=database_list, default=0)
+    db = SelectField(label=u'Database:', choices=database_list, default=0)
     submit = SubmitField(u'Begin')
