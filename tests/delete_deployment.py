0c9ed9bffcbb8665e269929cd4bd6aa155b27e8f
Anders Olson <sredna43@gmail.com>
2019-10-27 16:59:43 -0500

    added a test deployment

--- tests/delete_deployment.py
+++ tests/delete_deployment.py
@@ -1,12 +1,16 @@
 from os import path
-import yaml
+import sys
 from kubernetes import client, config
 
 def main():
     config.load_kube_config()
     v1 = client.AppsV1Api()
-    resp = v1.delete_namespaced_deployment(name='cir-anders-openmp', namespace="cir-anders-namespace")
-    print("Deployment {} deleted.".format('cir-anders-openmp'))
+    if sys.argv[1]:
+        name = sys.argv[1]
+    else:
+        name = 'cir-anders-openmp'
+    resp = v1.delete_namespaced_deployment(name=name, namespace="cir-anders-namespace")
+    print("Deployment {} deleted.".format(name))
 
 if __name__ == '__main__':
     main()
